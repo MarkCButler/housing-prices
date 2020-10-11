8d6a0cb774eae4fdd89c8a30877004d039154087
Mark Butler <mrkcbutler@gmail.com>
2020-10-11 17:12:55 -0500

    Rework encode.R
    Add methods 'mean', 'random' for encoding the test data.

--- encode.R
+++ encode.R
@@ -1,5 +1,6 @@
 library(caret)
 library(dplyr)
+library(purrr)
 library(tibble)
 library(tidyr)
 
@@ -9,13 +10,21 @@ library(tidyr)
 # <10, a sigmoid function is used to take a weighted average of the
 # out-of-fold mean and the mean of the target over the training data.
 #
-# To encode a categorical variable of the test data, each observation receives
-# one of the k encodings generated for the same variable for training data,
-# and the choice is random.  In order to facilitate this process, a list is
-# used to store the k encodings for each variable that was encoded.  For
-# instance, variable_encodings$Neighborhood is a data frame with row names
-# equal to the unique categories for the Neightborhood variable and columns
-# equal to the k different encodings.
+# Two methods are implemented for encoding the test data:
+#
+# 1.  Given an observation and the class for that observation, randomly select
+# one of the k encodings generated for that class.
+#
+# 2.  Given a class, all observations of that class receive the mean of the k
+# encodings generated for the class  This is the natural choice for a linear
+# model, since it is equivalent to running many experiments using method 1 to
+# generate input and then taking the mean of the outputs.
+#
+# In order to facilitate the process of assigning encodings to the test data,
+# the list 'variable_encodings' is used to store all encodings.  For instance,
+# variable_encodings$Neighborhood is a data frame with row names equal to the
+# unique categories for the Neightborhood variable and columns equal to the k
+# different encodings.
 variable_encodings <- list()
 
 # Sigmoid function that varies from ~0 to ~1 in the range 0 to 2*center
@@ -28,7 +37,8 @@ sigmoid <- function(x, center) {
 }
 
 # Perform k-fold target encoding for the training data.
-encode <- function(data, variable_name, k = 5, sigmoid_center = 5) {
+encode <- function(data, variable_name, target_mean,
+                   k = 5, sigmoid_center = 5) {
     # To simplify dplyr commands, rename the column of categorical data to be
     # encoded.  Also replace NA by the string "missing" to simplify the
     # encoding process.
@@ -46,7 +56,6 @@ encode <- function(data, variable_name, k = 5, sigmoid_center = 5) {
     k_fold_encodings <- data.frame(category = unique(to_encode$category),
                                    stringsAsFactors = FALSE)
 
-    target_mean <- mean(to_encode$SalePrice)
     folds <- createFolds(to_encode$category, k = k)
     na_replacements <- list(count = 0, out_of_fold_mean = 0)
     for (index in seq_along(folds)) {
@@ -87,18 +96,39 @@ encode <- function(data, variable_name, k = 5, sigmoid_center = 5) {
     return(data)
 }
 
-# Assign encodings to a variable of the test data, choosing randomly for each
-# observation between the k different encodings found for the same variable in
-# the training data.
-assign_encoding <- function(data, variable_name) {
+# Assign encodings to a variable of the test data.  In general the k-fold
+# encoding gives k different values, and a method is needed to assign an
+# encoding to a particular observation based on those values.  The method
+# argument selects one of two methods:
+#
+# 1.  'mean':  take the mean of the k values
+#
+# 2.  'random':  randomly select one of the k values
+#
+assign_encoding <- function(data, variable_name, target_mean, method) {
     to_encode <- data[[variable_name]]
     encoding <- variable_encodings[[variable_name]]
-    column_selection <- sample(ncol(encoding), size = nrow(data),
-                               replace = TRUE)
-    encoded_variable_name <- paste0(variable_name, 'num')
-    data[[encoded_variable_name]] <- map_dbl(
+    encoded_variable_name <- paste0(variable_name, 'Num')
+
+    if (method == 'random') {
+        column_selection <- sample(ncol(encoding), size = nrow(data),
+                                   replace = TRUE)
+    } else {
+        if (method != 'mean') {
+            stop('Unrecognized encoding method in assign_encoding.')
+        }
+        encoding <- data.frame(mean = rowMeans(encoding))
+        column_selection <- rep.int(1, times = nrow(data))
+    }
+
+    encoded_data <- map_dbl(
         seq_along(to_encode),
         ~ encoding[to_encode[.], column_selection[.]]
     )
+
+    data[[encoded_variable_name]] <- replace(
+        encoded_data, is.na(encoded_data), target_mean
+    )
+
     return(data)
 }
